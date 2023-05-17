02959211f39da6733938f6f31c4d162a6eb36740
zolantal <zoltan_antal@outlook.com>
2023-05-17 16:05:53 +0200

    Add functionality to reverseString()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let newString = "";
 
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+
+return newString;
 };
 
 // Do not edit below this line
