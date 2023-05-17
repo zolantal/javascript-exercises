4a4487f4df858d5e4e17d0a3256af58b5b2714c8
zolantal <zoltan_antal@outlook.com>
2023-05-17 16:40:30 +0200

    Add functionality to sumAll()

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let sum = 0;
 
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
