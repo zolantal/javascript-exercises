697ce33d4d4fb60b4b1f9dd0453166576d5f75d1
zolantal <zoltan_antal@outlook.com>
2023-05-17 16:53:39 +0200

    Add functionality to convertToCelsius()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,8 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  return Math.round(((x - 32) * 5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
 };
 
 // Do not edit below this line
