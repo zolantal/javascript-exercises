315e80ca99b61880d27cead28b363e64610f159b
zolantal <zoltan_antal@outlook.com>
2023-05-17 16:00:01 +0200

    Add functionality to repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+  if (n < 0) {
+    return "ERROR"
+  }
 
+  newString = "";
+
+  for (let i = 0; i < n; i++) {
+    newString += string;
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
