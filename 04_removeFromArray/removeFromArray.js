01a7e5b368bffd1ab007c99a382dff4e78148443
zolantal <zoltan_antal@outlook.com>
2023-05-17 16:32:41 +0200

    Add functionality to removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+  // for (const i of toRemove) {
+  //   array = array.filter(x => x!==i);
+  // }
 
+  // return array;
+
+  return array.filter(x => !toRemove.includes(x))
 };
 
 // Do not edit below this line
