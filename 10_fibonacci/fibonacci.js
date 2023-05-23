ed1ca13656fe302587d32ce6c41186b00d59332b
zoltan-antal <zoltan_antal@outlook.com>
2023-05-23 13:40:50 +0200

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (Number(n) <= 0) {
+    return "OOPS";
+  }
 
+  return fibonacci_aux(1, 1, Number(n));
+  
+  function fibonacci_aux(a, b, n) {
+    if (n === 1) {
+      return 1;
+    } else if (n === 2) {
+      return b;
+    } else {
+      return fibonacci_aux(b, a + b, n - 1)
+    }
+  }
 };
 
 // Do not edit below this line
