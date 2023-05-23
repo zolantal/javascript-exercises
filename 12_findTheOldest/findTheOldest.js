3dedea27100c395a5081e1e135a63f80f5c17fe0
zoltan-antal <zoltan_antal@outlook.com>
2023-05-23 13:54:20 +0200

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.sort((a, b) => {
+    const aBirth = a.yearOfBirth;
+    let aDeath = a.yearOfDeath;
+    const bBirth = b.yearOfBirth;
+    let bDeath = b.yearOfDeath;
 
+    if (!aDeath) {
+      aDeath = new Date().getFullYear();
+    }
+    if (!bDeath) {
+      bDeath = new Date().getFullYear();
+    }
+
+    return (aDeath - aBirth) > (bDeath - bBirth) ? -1 : 1;
+  })[0];
 };
 
 // Do not edit below this line
