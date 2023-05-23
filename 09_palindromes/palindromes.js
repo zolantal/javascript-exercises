d637fbcff103f2ae2d78a9ea8cf3a7e30770dc60
zoltan-antal <zoltan_antal@outlook.com>
2023-05-23 13:25:17 +0200

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  characters = Array.from(string);
+  letters = characters.filter(char => char.toUpperCase() !== char.toLowerCase());
 
+  palindrome = true;
+  for (let i = 0; i < Math.floor(letters.length / 2); i++) {
+    if (letters[i].toLowerCase() !== letters[letters.length - 1 - i].toLowerCase()) {
+      palindrome = false;
+    }    
+  }
+
+  return palindrome;
 };
 
 // Do not edit below this line
